c8ef2d5abe115aad3a24ece87fbb8241e2d9e268
Domenico Paolo <domenicopaolo.plati@gmail.com>
2022-06-11 22:25:06 +0200

    Frontend added

--- src/main/java/com/example/pswproject/PswProjectApplication.java
+++ src/main/java/com/example/pswproject/PswProjectApplication.java
@@ -2,8 +2,9 @@ package com.example.pswproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class PswProjectApplication {
 
     public static void main(String[] args) {
